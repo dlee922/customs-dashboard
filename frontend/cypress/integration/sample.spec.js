98d6ec0ce1cc0ca4d62c035bc5c13836c2ae9dfc
Daniel Lee <danielhle922@gmail.com>
2025-02-05 14:55:23 -0500

    remove sample cypress tests

--- frontend/cypress/integration/sample.spec.js
+++
@@ -1,7 +0,0 @@
-describe('Sample Test', () => {
-  it('Visits the Kitchen Sink', () => {
-    cy.visit('https://example.cypress.io');
-    cy.contains('type').click();
-    cy.url().should('include', '/commands/actions');
-  });
-});
